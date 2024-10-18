c934ad78cf510c916910a007e613eb8c6778a96a
Joh-Ishimwe <j.ishimwe3@alustudent.com>
2024-10-18 13:20:24 +0200

    Terms and condiction page initial push

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:share_plate/UI/fogot_password.dart';
-import 'package:share_plate/UI/signin_page.dart';
+// import 'package:share_plate/UI/signin_page.dart';
+import 'package:share_plate/UI/terms.dart';
 
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignInPage(),
+      home: Terms(),
     );
   }
 }
