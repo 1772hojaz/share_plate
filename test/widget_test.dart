f67c77618c754dcca3ba852b41640002fc7e37bd
John Akech <j.akech@alustudent.com>
2024-10-20 08:27:39 +0200

    HomePageScreen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:donor_home/main.dart';
+// import 'package:donor_home/main.dart';
+import 'package:share_plate/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
