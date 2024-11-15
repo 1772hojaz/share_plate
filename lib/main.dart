cda2d22ee9bd93994961e0ae1004017db4f3db0c
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-11-15 09:13:38 +0200

    fire base intergration

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
-
-import 'package:share_plate/UI/feedback_page.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:share_plate/UI/feedback_page.dart';
 import 'UI/food_listing.dart';
 import 'splash_screen.dart';
 import 'UI/signin_page.dart';
 import 'UI/terms.dart';
 import 'UI/fogot_password.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,21 +21,20 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) 
-
+  Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: "Share Plate",
-      initialRoute: "/",
+      title: 'Share Plate',
+      initialRoute: '/',
       getPages: [
-        GetPage(name: "/", page: () => SplashScreen()),
-        GetPage(name: "/home", page: () => FoodListingPage()),
-        GetPage(name: '/signin', page: () => SignInPage()),
-        GetPage(name: '/signin', page: () => SignInPage()),
-        GetPage(name: '/fogotpassword', page: () => ForgotPasswordScreen())
+        GetPage(name: '/', page: () => const SplashScreen()),
+        GetPage(name: '/home', page: () => FoodListingPage()),
+        GetPage(name: '/signin', page: () => const SignInPage()),
+        GetPage(
+            name: '/forgotpassword', page: () => const ForgotPasswordScreen()),
+        GetPage(name: '/feedback', page: () => const FeedbackPage()),
+        GetPage(name: '/terms', page: () => TermsPage()),
       ],
       debugShowCheckedModeBanner: false,
-
-
     );
   }
 }
